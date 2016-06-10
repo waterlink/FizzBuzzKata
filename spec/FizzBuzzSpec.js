33c0a4f7bbeec0481c1d36bb4c2fb7d9a7572b1e
Ilona Demidenko <demiluri@gmail.com>
2016-06-10 21:46:05 +0200

    Implement sequence with Fizz

--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -1,4 +1,27 @@
-it("is Fizz when a number divides by 3", function() {
-  var sequence = makeFizzBuzzSequence()
-  expect(sequence[2]).toEqual("Fizz")
+describe("makeFizzBuzzSequence()", function() {
+  var sequence
+
+  beforeEach(function() {
+    sequence = makeFizzBuzzSequence()
+  })
+
+  it("is 1 when the number is 1", function() {
+    expect(sequence[0]).toEqual("1")
+  })
+
+  it("is 2 when the number is 2", function() {
+    expect(sequence[1]).toEqual("2")
+  })
+
+  it("is 19 when the number is 19", function() {
+    expect(sequence[18]).toEqual("19")
+  })
+
+  it("is Fizz when the number is 3", function() {
+    expect(sequence[2]).toEqual("Fizz")
+  })
+
+  it("is Fizz when the number is 6", function() {
+    expect(sequence[5]).toEqual("Fizz")
+  })
 })
--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,4 +1,11 @@
 function makeFizzBuzzSequence() {
-var array = [1, 2, "Fizz"]
+  var array = []
+  for (var i = 1; i <= 100; i++) {
+    if (i % 3 == 0) {
+      array.push("Fizz")
+    } else {
+      array.push(i.toString())
+    }
+  }
   return array
 }
